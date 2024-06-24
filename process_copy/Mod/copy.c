f330fe5f0a9c129c0206c4df5d7ef3703e911fa8
20230912wjx <3495169862@qq.com>
2024-06-24 22:02:34 +0800

    工程文件

---
+++ process_copy/Mod/copy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+
+
+
+
+
+
+
+
+
+
